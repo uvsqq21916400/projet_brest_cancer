b10271c28ad077dd73c2421ad9f2a8bcde5ce367
uvsqq21916400 <flaviekutukialanda@gmail.com>
2025-02-22 01:16:06 +0100

    Ajout du dataset et du script d'importation

---
+++ brest cancer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 21 23:24:38 2025
+
+@author: flavi
+"""
+
+import pandas as pd
+df=pd.read_csv("C:/Users/flavi/Downloads/breast-cancer-dataset.csv")
+
+df.head()
+df.info()
+
